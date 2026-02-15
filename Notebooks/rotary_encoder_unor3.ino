e8391626fb2d9ff57a3aea5955025205007d0b69
Max Vazquez <max21504128@gmail.com>
2026-02-15 15:12:01 -0600

    Finished Encoder IO code

--- Notebooks/rotary_encoder_unor3.ino
+++ Notebooks/rotary_encoder_unor3.ino
@@ -1,62 +1,56 @@
+#include <Arduino.h>
 #include <RotaryEncoder.h>
 
-#define ENCODER_PIN_A 2  // Interrupt-capable pin A
-#define ENCODER_PIN_B 3  // Interrupt-capable pin B
+#define ENCODER_PIN_A 2
+#define ENCODER_PIN_B 3
 
-#define ENC_STEPS_PER_ROTATION 1200  // Adjust based on your encoder specs
+// Measured with your calibration: 600 counts per mechanical revolution
+#define COUNTS_PER_REV 600L
 
-RotaryEncoder encoder(ENCODER_PIN_A, ENCODER_PIN_B, RotaryEncoder::LatchMode::TWO03);
+RotaryEncoder encoder(ENCODER_PIN_A, ENCODER_PIN_B, RotaryEncoder::LatchMode::FOUR3);
+
+void encoderISR() { encoder.tick(); }
+
+static inline long readEncoderAtomic() {
+  long p;
+  noInterrupts();
+  p = encoder.getPosition();
+  interrupts();
+  return p;
+}
+
+float get_pendulum_angle_deg() {
+  long pos = readEncoderAtomic();
+
+  // Convert counts -> degrees (unwrapped)
+  float angle = (float)pos * (360.0f / (float)COUNTS_PER_REV);
+
+  // Wrap to [-180, 180)
+  while (angle >= 180.0f) angle -= 360.0f;
+  while (angle < -180.0f) angle += 360.0f;
+
+  // If you prefer [0, 360), comment the wrap above and use this instead:
+  // while (angle >= 360.0f) angle -= 360.0f;
+  // while (angle < 0.0f) angle += 360.0f;
+
+  return angle;
+}
 
 void setup() {
   Serial.begin(115200);
 
-  encoder.setPosition(0);
-
   pinMode(ENCODER_PIN_A, INPUT_PULLUP);
   pinMode(ENCODER_PIN_B, INPUT_PULLUP);
+
+  encoder.setPosition(0); // put pendulum DOWN when powering up (or when you want zero)
 
   attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A), encoderISR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_B), encoderISR, CHANGE);
 }
 
 void loop() {
-  static int lastPosition = 0;
-  int currentPosition = encoder.getPosition();
-
-  if (currentPosition != lastPosition) {
-    float angle = get_encoder_angle();
-    //Serial.print("Position: ");
-    //Serial.print(currentPosition);
-    Serial.print("  Angle: ");
-    Serial.println(angle, 2);
-    lastPosition = currentPosition;
-  }
-
+  float angle = get_pendulum_angle_deg();
+  Serial.print("Angle: ");
+  Serial.println(angle, 2);
   delay(50);
-}
-
-void encoderISR() {
-  encoder.tick();
-}
-/**
-float get_encoder_angle() {
-  int pos = encoder.getPosition();
-
-  pos = pos % ENC_STEPS_PER_ROTATION;
-  if (pos < 0) pos += ENC_STEPS_PER_ROTATION;
-
-  return (float)pos * (360.0 / ENC_STEPS_PER_ROTATION);
-}
-*/
-
-float get_encoder_angle() {
-  int pos = encoder.getPosition();
-
-  pos = pos % ENC_STEPS_PER_ROTATION;
-  if (pos < 0) pos += ENC_STEPS_PER_ROTATION;
-
-  float angle = (float)pos * (360.0 / ENC_STEPS_PER_ROTATION);
-  angle = fmod(angle + 180.0, 360.0) - 180.0;  // Wrap angle to [-180, 180)
-
-  return angle;
 }
